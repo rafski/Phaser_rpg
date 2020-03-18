1f41abb0aea558f3646fb2276bc71b3a43fed1b8
Raf <rkrassowski@gmail.com>
2020-03-18 10:07:53 +0000

    Chunking the game into individual scenes

---
+++ assets/js/scenes/BootScene.js
@@ -0,0 +1,13 @@
+class BootScene extends Phaser.Scene{
+    constructor() {
+        super('Boot');
+    }
+
+    preload() {
+
+    }
+
+    create() {
+        
+    }
+}
---
+++ assets/js/scenes/UIScene.js
@@ -0,0 +1,13 @@
+class UIScene extends Phaser.Scene{
+    constructor() {
+        super('UI');
+    }
+
+    preload() {
+
+    }
+
+    create() {
+        
+    }
+}
